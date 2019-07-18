178431b4094227a980f4120c588e722e59e440f1
Rohan Bhanot <rohanbhanot@Rohans-MacBook-Pro.local>
2019-07-18 13:05:07 -0700

    revised wrapper

--- heatmap4/R/heatmap4.R
+++ heatmap4/R/heatmap4.R
@@ -93,7 +93,7 @@ heatmap4 <- function(){
   heatmap4(x = genomDat[probeId, ], Rowv = NA, Colv = NULL, distfun = dist, hclustfun = hclust,  symm = FALSE,
                   ColSideColors = samCol, RowSideColors = chrCol[probeId], labCol = colnames(genomDat), labRow = NA,
                   scale = "none", na.rm = FALSE, margins = c(5, 5), main = NULL, xlab = NULL, ylab = NULL, zlm = limit,
-                  high = cols[1], low = cols[2], mid = cols[3], addamps = amplifDat[probeId,], colamps = "yellow",
+                  high = cols[1], low = cols[2], mid = cols[3], addamps = amplifDat[probeId, ], colamps = "yellow",
                   addText = countDat)
 }
 
