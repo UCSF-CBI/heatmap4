4a0079c49d714d185a45902d630b3c1a1ae404e1
Rohan Bhanot <rohanbhanot@Rohans-MBP.ucsf.edu>
2019-07-25 12:15:33 -0700

    latest wrapper

---
+++ heatmap4/R/general_heatmap.R
@@ -0,0 +1,163 @@
+
+## Calling Bioconductor
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("marray")
+load("data.RData")
+
+## Calls Libraries
+library(RColorBrewer)
+library(marray)
+
+## Calls Files
+source("heatmap4.R")
+source("heatmapRelated.R")
+
+## Tileplot
+
+
+## Replacing Labels
+row_lab_vtr <- NULL
+col_lab_vtr <- NULL
+
+## Main Data Matrix
+x <- NULL
+
+## Row and Column Names
+row_new_lab <- rownames(x) ## Name of main matrix
+col_new_lab <- colnames(x) ## Name of main matrix
+
+if(!(is.null(row_lab_vtr) & is.null(col_lab_vtr))) {
+  row_lab_vtr <- row_new_lab
+  col_lab_vtr <- col_new_lab
+}
+
+## Annotation Variables
+row_info <- NULL
+as.data.frame(row_info)
+
+col_info <- NULL
+as.data.frame(col_info)
+
+## Wrapped heatmap function
+generate_heatmap <- function(x, row_info, col_info, row_anno = c(TRUE, FALSE), col_anno = c(TRUE, FALSE), row_lab = c(TRUE, FALSE),
+                            col_lab = c(TRUE, FALSE), row_clust = c(TRUE, FALSE), col_clust = c(TRUE, FALSE),
+                            row_dend = c(TRUE, FALSE), col_dend = c(TRUE, FALSE))
+  {
+  ## Reducing Matrices
+  i <- 1:20
+  j <- 1:10
+
+  ## Simplifying Data Matrices
+  x <- x[i, j]
+  row_info <- row_info[i, ]
+  col_info <- col_info[, j]
+
+  ## Random sample matrix that is the same size as genomDat
+  #countDat <- matrix(as.character(sample(1:10, nrow(x) * ncol(x), replace = TRUE)),
+                     #nrow = nrow(x), ncol = ncol(x))
+
+  #countDat[2, 1:4] <- NA
+
+  ## Annotations
+  row_anno <- row_anno[1]
+
+  if(row_anno) {
+    row_var <- as.vector(row_info)
+
+    color_vec <- c("skyblue", "blue", "yellow", "purple", "black", "red", "orange", "green", "cyan", "darkgreen")
+    if (is.null(row_var)) {
+      row_color <- color_vec
+    }
+    for (anno in row_var) {
+      a <- 1
+      row_color <- c(color_vec[a])
+      a <- a + 1
+    }
+
+    RowSideColors <- row_color
+  }
+  else {
+    RowSideColors <- NA
+  }
+
+  col_anno <- col_anno[1]
+
+  if(col_anno) {
+    col_info <- colnames(anno_table)
+    col_var <- as.vector(col_info)
+
+
+    ColSideColors <- samCol
+  }
+  else {
+    ColSideColors <- NA
+  }
+
+  ## Labels
+  row_lab <- row_lab[1]
+
+  if(row_lab) {
+    labRow <- row_lab_vtr
+  }
+  else {
+    labRow <- NA
+  }
+
+  col_lab <- col_lab[1]
+
+  if(col_lab) {
+    labCol <- col_lab_vtr
+  }
+  else {
+    labCol <- NA
+  }
+
+  ## Clusters
+  row_clust <- row_clust[1]
+
+  if(row_clust) {
+    #arguement
+  }
+  else {
+    #arguement
+  }
+
+  col_clust <- col_clust[1]
+
+  if(col_clust) {
+    #arguement
+  }
+  else {
+    #arguement
+  }
+
+  ## Dendograms
+  row_dend <- row_dend[1]
+
+  if(row_dend) {
+    Rowv <- NULL
+  }
+  else {
+    Rowv <- NA
+  }
+
+  col_dend <- col_dend[1]
+
+  if(col_dend) {
+    Colv <- NULL
+  }
+  else {
+    Colv <- NA
+  }
+
+    ## clusters need to be set
+
+  heatmap4(x = x, Rowv = Rowv, Colv = Colv, distfun = dist, hclustfun = hclust,  symm = FALSE,
+            ColSideColors = ColSideColors, RowSideColors = RowSideColors, labCol = labCol, labRow = labRow,
+            scale = "none", na.rm = FALSE, margins = c(5, 5), main = NULL, xlab = NULL, ylab = NULL, zlm = limit,
+            high = cols[1], low = cols[2], mid = cols[3], addamps = amplifDat[probeId, ], colamps = "yellow",
+            addText = countDat)
+  }
+
