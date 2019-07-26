341f880c6238168ab4f9b484a2c6d38dd07fcb7e
Rohan Bhanot <rohanbhanot@Rohans-MacBook-Pro.local>
2019-07-25 17:27:00 -0700

    cleaned code

--- heatmap4/R/general_heatmap.R
+++ heatmap4/R/general_heatmap.R
@@ -14,38 +14,31 @@ library(marray)
 source("heatmap4.R")
 source("heatmapRelated.R")
 
-## Tileplot
-
-
-## Replacing Labels
-row_lab_vtr <- NULL
-col_lab_vtr <- NULL
-
-## Main Data Matrix
-x <- NULL
-
-## Row and Column Names
-row_new_lab <- rownames(x) ## Name of main matrix
-col_new_lab <- colnames(x) ## Name of main matrix
-
-if(!(is.null(row_lab_vtr) & is.null(col_lab_vtr))) {
-  row_lab_vtr <- row_new_lab
-  col_lab_vtr <- col_new_lab
-}
-
-## Annotation Variables
-row_info <- NULL
-as.data.frame(row_info)
-
-col_info <- NULL
-as.data.frame(col_info)
-
-probeId
 ## Wrapped heatmap function
-generate_heatmap <- function(x, row_info, col_info, row_anno = c(TRUE, FALSE), col_anno = c(TRUE, FALSE), row_lab = c(TRUE, FALSE),
-                            col_lab = c(TRUE, FALSE), row_clust = c(TRUE, FALSE), col_clust = c(TRUE, FALSE),
-                            row_dend = c(TRUE, FALSE), col_dend = c(TRUE, FALSE))
+generate_heatmap <- function(x, row_info = NULL, col_info = NULL, row_anno = c(TRUE, FALSE), col_anno = c(TRUE, FALSE),
+                             row_lab = c(TRUE, FALSE), col_lab = c(TRUE, FALSE), row_clust = c(TRUE, FALSE),
+                             col_clust = c(TRUE, FALSE), row_dend = c(TRUE, FALSE), col_dend = c(TRUE, FALSE),
+                             col_var_info = list(NULL), row_var_info = list(NULL), test_scenario = c(TRUE, FALSE))
   {
+
+  #NEED TO FIND PURPOSE, or delete
+  ## Replacing Labels
+  row_lab_vtr <- NULL
+  col_lab_vtr <- NULL
+
+  ## Row and Column Names
+  row_new_lab <- rownames(x) ## Name of main matrix
+  col_new_lab <- colnames(x) ## Name of main matrix
+
+  if(!(is.null(row_lab_vtr) & is.null(col_lab_vtr))) {
+    row_lab_vtr <- row_new_lab
+    col_lab_vtr <- col_new_lab
+  }
+
+
+  ##Test Scenario
+  test_scenario <- test_scenario[1]
+  if (test_scenario) {
   ## Reducing Matrices
   i <- 1:20
   j <- 1:10
@@ -54,19 +47,13 @@ generate_heatmap <- function(x, row_info, col_info, row_anno = c(TRUE, FALSE), c
   x <- x[i, j]
   row_info <- row_info[i, ]
   col_info <- col_info[, j]
-  #amplifDat <- amplifDat[i, j]
-
-  ## Random sample matrix that is the same size as genomDat
-  countDat <- matrix(as.character(sample(1:10, nrow(x) * ncol(x), replace = TRUE)),
-                     nrow = nrow(x), ncol = ncol(x))
-
-  countDat[2, 1:4] <- NA
+  }
 
   ## Annotations
-  row_anno <- row_anno[1]
+  row_anno <- row_anno[2]
 
   if(row_anno) {
-    row_var <- as.vector(row_info)
+    row_var <- colnames(row_info)
 
     color_vec <- c("skyblue", "blue", "yellow", "purple", "black", "red", "orange", "green", "cyan", "darkgreen")
     if (is.null(row_var)) {
@@ -84,7 +71,7 @@ generate_heatmap <- function(x, row_info, col_info, row_anno = c(TRUE, FALSE), c
     RowSideColors <- NA
   }
 
-  col_anno <- col_anno[1]
+  col_anno <- col_anno[2]
 
   if(col_anno) {
     col_var <- as.vector(col_info)
@@ -129,7 +116,7 @@ generate_heatmap <- function(x, row_info, col_info, row_anno = c(TRUE, FALSE), c
   row_clust <- row_clust[1]
 
   if(row_clust) {
-    cluster <- hclust()
+    cluster <- hclust(dist(as.matrix(x)), method = "complete", members = NULL)
   }
   else {
     cluster <-  NA
@@ -138,7 +125,7 @@ generate_heatmap <- function(x, row_info, col_info, row_anno = c(TRUE, FALSE), c
   col_clust <- col_clust[1]
 
   if(col_clust) {
-    cluster <- hclust()
+    cluster <- hclust(dist(as.matrix(x)), method = "complete", members = NULL)
   }
   else {
     cluster <- NA
@@ -170,6 +157,7 @@ generate_heatmap <- function(x, row_info, col_info, row_anno = c(TRUE, FALSE), c
             ColSideColors = ColSideColors, RowSideColors = RowSideColors, labCol = labCol, labRow = labRow,
             scale = "none", na.rm = FALSE, margins = c(5, 5), main = NULL, xlab = NULL, ylab = NULL, zlm = limit,
             high = cols[1], low = cols[2], mid = cols[3])
-  #addamps = amplifDat[probeId, ], colamps = "yellow", addText = countDat
   }
 
+generate_heatmap(genomDat, row_info = chrInfo, col_info = phen, row_anno = FALSE, col_anno = TRUE, row_lab = TRUE,
+                 col_lab = TRUE, row_dend = FALSE)
